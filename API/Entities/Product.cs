966d0c9cd7d1d0c5e483013275a5557b01dc6b32
Miles Koller <mileskoller@gmail.com>
2023-03-23 13:04:30 -0500

    Db init and DB seed data

--- API/Entities/Product.cs
+++ API/Entities/Product.cs
@@ -4,8 +4,8 @@ namespace API.Entities
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int Description { get; set; }
-        public long Price { get; set; }
+        public string Description { get; set; }
+        public long Price { get; set; } 
         public string PictureUrl { get; set; }
         public string Type { get; set; }
         public string Brand { get; set; }
