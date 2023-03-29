76b58b22f0064031e79b59d6444a606aacc84bcd
Miles Koller <mileskoller@gmail.com>
2023-03-29 16:05:23 -0500

    use state hook

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+
 function App() {
+  const [products, setProducts]  = useState([
+    {name: 'product1', price: 100.00},
+    {name: 'product2', price: 200.00},
+  ]);
+
+  function addProduct() {
+    setProducts(prevState => [...prevState, 
+      {name: 'product' + (prevState.length +1), price: (prevState.length *100) +100}])
+  }
+
   return (
- <div>
-  <h1>Res-Store</h1>
- </div>
+    <div>
+      <h1> Re-Store</h1>
+      <ul>
+        {products.map((item, index) => (
+          <li key={index}>{item.name} - {item.price}</li>
+        ))}
+      </ul>
+      <button onClick={addProduct}>Add Product</button>
+    </div>
   );
 }
 
