9888af444c7a842626c3b89d977d88985ad37c9d
Miles Koller <mileskoller@gmail.com>
2023-04-18 12:13:34 -0500

    catalog component

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { Product } from "../models/product";
+import Catalog from "../../features/catalog/Catalog";
 
 function App() {
   const [products, setProducts]  = useState<Product []>([]);
@@ -25,12 +26,7 @@ function App() {
   return ( //returns products in application
     <div>
       <h1> Re-Store</h1>
-      <ul>
-        {products.map((product) => ( // knows its a products array
-          <li key={product.id}>{product.name} - {product.price}</li>
-        ))}
-      </ul>
-      <button onClick={addProduct}>Add Product</button>
+      <Catalog products={products} addProduct={addProduct} />
     </div>
   );
 }
