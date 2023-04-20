8e2e139cb5230012bb6ad828ebe2a9fbd7540d3a
Miles Koller <mileskoller@gmail.com>
2023-04-20 14:27:35 -0500

    toast errors

--- client/src/app/api/agent.ts
+++ client/src/app/api/agent.ts
@@ -1,13 +1,41 @@
 import axios, { AxiosError, AxiosResponse } from "axios";
+import { toast } from "react-toastify";
+import { router } from "../router/Routes";
+
+const sleep = () => new Promise(resolve => setTimeout(resolve, 500))
 
 axios.defaults.baseURL = 'http://localhost:5000/api/';
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-axios.interceptors.response.use(response => {
+axios.interceptors.response.use(async response => {
+    await sleep();
     return response
 }, (error: AxiosError) => {
-    console.log('caught by axios interceptor!!');
+    const {data, status} = error.response as AxiosResponse;
+    switch (status) {
+        case 400:
+            if (data.errors) {
+                const modelStateErrors: string[] = [];
+                for (const key in data.errors) {
+                    if (data.errors[key]) {
+                        modelStateErrors.push(data.errors[key])
+                    }
+                }
+                throw modelStateErrors.flat();
+            }
+            toast.error(data.title);
+            break;
+        case 401:
+            toast.error(data.title);
+            break;
+        case 500:
+            router.navigate('/server-error', {state: {error: data}});
+            break;
+        default:
+            break;
+    }
+
     return Promise.reject(error.response);
 })
 
@@ -15,7 +43,7 @@ const requests = {
     get: (url: string) => axios.get(url).then(responseBody),
     post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
     put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(responseBody),
+    del: (url: string) => axios.delete(url).then(responseBody)
 }
 
 const Catalog = {
@@ -23,12 +51,12 @@ const Catalog = {
     details: (id: number) => requests.get(`products/${id}`)
 }
 
-const TestErrors  = {
-    get400Error: () => requests.get('buggy/bad-request,'),
-    get401Error: () => requests.get('buggy/unauthorized,'),
-    get404Error: () => requests.get('buggy/not-found,'),
-    get500Error: () => requests.get('buggy/server-error,'),
-    getValidationError: () => requests.get('buggy/validation-error,'),
+const TestErrors = {
+    get400Error: () => requests.get('buggy/bad-request'),
+    get401Error: () => requests.get('buggy/unauthorised'),
+    get404Error: () => requests.get('buggy/not-found'),
+    get500Error: () => requests.get('buggy/server-error'),
+    getValidationError: () => requests.get('buggy/validation-error')
 }
 
 const agent = {
