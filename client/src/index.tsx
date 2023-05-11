706b2f6137ae1b4c8c998d32b9af30d8598818c5
Miles Koller <mileskoller@gmail.com>
2023-05-11 16:49:54 -0500

    redux basket slice

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,17 +7,16 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { router } from './app/router/Routes';
 import { RouterProvider } from 'react-router-dom';
-import { StoreProvider } from './app/context/StoreContext';
-import { configureStore } from './app/store/configureStore';
+import { router } from './app/router/Routes';
 import { Provider } from 'react-redux';
-
-const store = configureStore();
+import { store } from './app/store/configureStore';
+import { StoreProvider } from './app/context/StoreContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <StoreProvider>
